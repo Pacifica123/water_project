e3a1dc2539246b9f2aab09d3ac8dd12bd511000e
Ваше Имя <you@example.com>
2025-02-08 16:20:07 +0700

    что-то с токеном авторизации

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css"; // Стили для дизайна, похожего на пример
 import LoginPage from "./components/LoginPage";
 import ProtectedContent from "./components/ProtectedContent"; // Основной компонент приложения
+import axios from 'axios';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -17,6 +18,23 @@ function App() {
   const handleLogin = () => {
     setIsAuthenticated(true);
   };
+
+
+  const axiosInstance = axios.create({
+    baseURL: 'http://127.0.0.1:5000/api', // Укажите ваш базовый URL
+  });
+
+  // Перехватчик для обработки ошибок
+  axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response && error.response.status === 401) {
+        // Здесь вызываем функцию выхода из сессии
+        handleLogout();
+      }
+      return Promise.reject(error);
+    }
+  );
 
   const handleLogout = () => {
     console.log("Выход из системы");
